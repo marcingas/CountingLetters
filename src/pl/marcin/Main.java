7dcbd484c44ad2b30aa314219225f682254ba99b
Marcin <gasiorek.mail@gmail.com>
2023-04-13 16:13:18 +0200

    Invocation print number of words

---
+++ src/pl/marcin/Main.java
@@ -0,0 +1,44 @@
+package pl.marcin;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<Character, Integer> numberOfWorsPerLetter = new HashMap<>();
+        String userInput = "Litwo, Ojczyzno moja! ty jesteś jak zdrowie." +
+                " Ile cię trzeba cenić,ten tylko się dowie," +
+                " Kto cię stracił.";
+        userInput = userInput.replaceAll(",", " ").replaceAll("!", " ")
+                .replaceAll("\\.", " ").replaceAll("  ", " ").toLowerCase();
+
+        String[] wordsInInvocationAsArray = userInput.split(" ");
+        List<String>wordsInINvocationAsList = Arrays.asList(wordsInInvocationAsArray);
+
+        for(String w : wordsInINvocationAsList){
+           Character firstLetter;
+            String word = w;
+            firstLetter = word.charAt(0);
+            if(numberOfWorsPerLetter.containsKey(firstLetter)){
+                int value = numberOfWorsPerLetter.get(firstLetter);
+                numberOfWorsPerLetter.put(firstLetter,value + 1);
+            }else{
+                numberOfWorsPerLetter.put(firstLetter,1);
+
+            }
+
+        }
+        System.out.println("W pierwszych dwóch zdaniach inwokacji znajduje się tyle słów rozpoczynających się " +
+                " na konkretną literę: ");
+
+        for(Map.Entry<Character,Integer> entry : numberOfWorsPerLetter.entrySet()){
+            Character key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println(key + " -> " + value);
+        }
+
+    }
+}
